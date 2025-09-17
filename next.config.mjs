75a6f5241e8009dac3d3118c85f04160bee0e792
Con-Ses <connorsessions@gmail.com>
2025-09-16 20:05:43 -0700

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',          // generates static files into /out
-  trailingSlash: true,       // makes /route/ => /route/index.html (better for static hosting)
-  images: { unoptimized: true }, // keep this for export
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-}
-export default nextConfig
